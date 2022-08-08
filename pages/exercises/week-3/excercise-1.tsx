133b0570d8985aa44d19415c504e77efb03284c1
MarcinParda <marcin98parda@gmail.com>
2022-08-08 11:21:47 +0200

    displaing product list

--- pages/exercises/week-3/excercise-1.tsx
+++ pages/exercises/week-3/excercise-1.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Footer } from '../../../components/Footer';
 import { Header } from '../../../components/Header';
 
 interface StoreApiResponse {
@@ -56,7 +56,27 @@ const Main = () => {
     return <div>Something went wrong.</div>;
   }
 
-  return <div>Main</div>;
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <ul className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <li key={product.id}>
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <img src={product.image} alt={product.title} />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                ${product.price}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Exercise1Page;
