ab02a0de0b6f823742f7975bf861826f3c5e099c
MarcinParda <marcin98parda@gmail.com>
2022-10-22 22:56:11 +0200

    fix images displaying

--- components/Product.tsx
+++ components/Product.tsx
@@ -11,7 +11,7 @@ export default function Product({ image, price, title }: Props) {
   return (
     <>
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-        <Image src={image} alt={title} />
+        <Image src={image} alt={title} width={400} height={400} />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['tailwindui.com', 'images.unsplash.com'],
+    domains: [
+      'tailwindui.com',
+      'images.unsplash.com',
+      'naszsklep-api.vercel.app',
+    ],
   },
 };
 
