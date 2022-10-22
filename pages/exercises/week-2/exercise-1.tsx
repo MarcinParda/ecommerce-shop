64cc4c58240787251bc19c1b6fbe8e196d092916
MarcinParda <marcin98parda@gmail.com>
2022-10-22 22:41:01 +0200

    fix exercise 1 week 2

--- pages/exercises/week-2/exercise-1.tsx
+++ pages/exercises/week-2/exercise-1.tsx
@@ -1,12 +1,9 @@
-import { Footer } from 'components/Footer';
-import { Header } from 'components/Header';
-
 const AboutPage = () => {
   return (
     <>
-      <Header />
-      <main className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">Main</main>
-      <Footer />
+      <main className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        Code moved to Layout.tsx
+      </main>
     </>
   );
 };
