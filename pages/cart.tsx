69e190fb3e7fa90021576a6159d095abe8a95675
MarcinParda <marcin98parda@gmail.com>
2022-11-06 18:30:53 +0100

    feat(cart): :art: styled cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,16 +1,49 @@
+import { TrashIcon } from '@heroicons/react/outline';
 import { useCartState } from 'components/Header/Cart/CartContext';
 
-const CartPage = () => {
+const CartContent = () => {
   const cartState = useCartState();
   return (
-    <div>
-      <ul>
+    <div className="col-span-2">
+      <ul className="divide-y divide-gray-200">
         {cartState.items.map((item, index) => (
-          <li key={`${item.title}_${index}`}>
-            {item.title} - {item.price}
+          <li
+            key={`${item.title}_${index}`}
+            className="py-4 flex justify-between"
+          >
+            <div>{item.title}</div>
+            <div className="flex align-middle">
+              {item.price}$
+              <button className="ml-2 text-red-500">
+                <TrashIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
           </li>
         ))}
       </ul>
+    </div>
+  );
+};
+
+const CartSummary = () => {
+  const cartState = useCartState();
+  return (
+    <div>
+      Podsumowanie koszyka
+      <div className="font-bold">
+        Liczba elementów: {cartState.items.length}
+      </div>
+    </div>
+  );
+};
+
+const CartPage = () => {
+  return (
+    <div className="max-w-2xl mx-auto w-full p-4">
+      <div className="grid grid-cols-3 gap-8">
+        <CartContent />
+        <CartSummary />
+      </div>
     </div>
   );
 };
